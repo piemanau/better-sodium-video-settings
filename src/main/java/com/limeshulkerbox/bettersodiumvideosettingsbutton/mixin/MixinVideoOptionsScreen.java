baed8085e738b1e9aa2845513a7c0a6f785553ab
Grayray <69988482+Grayray75@users.noreply.github.com>
2021-02-06 21:48:55 +0100

    Remove unused imports

--- src/main/java/com/limeshulkerbox/bettersodiumvideosettingsbutton/mixin/MixinVideoOptionsScreen.java
+++ src/main/java/com/limeshulkerbox/bettersodiumvideosettingsbutton/mixin/MixinVideoOptionsScreen.java
@@ -1,17 +1,11 @@
 package com.limeshulkerbox.bettersodiumvideosettingsbutton.mixin;
 
 import me.jellysquid.mods.sodium.client.gui.SodiumOptionsGUI;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.VideoOptionsScreen;
-import net.minecraft.client.gui.screen.options.GameOptionsScreen;
-import net.minecraft.client.gui.screen.options.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.options.BooleanOption;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -33,10 +27,12 @@ public class MixinVideoOptionsScreen extends Screen {
             this.client.openScreen(new SodiumOptionsGUI(this));
         }));
     }
+
     @ModifyConstant(method = "init", constant = @Constant(intValue = 100, ordinal = 0))
     private int modifyDonePos(int input) {
         return 155;
     }
+
     @ModifyConstant(method = "init", constant = @Constant(intValue = 200, ordinal = 0))
     private int modifyDoneWidth(int input) {
         return 150;
