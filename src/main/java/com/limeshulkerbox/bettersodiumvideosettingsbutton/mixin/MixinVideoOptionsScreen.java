49fb3f16d98445c4eb2cad57549ea64e14b986a9
LimeShulkerBox <77601849+LimeShulkerBox@users.noreply.github.com>
2021-02-14 17:33:19 +1100

    Changed Video Settings Menu
    Removed 3 settings due to them not working, should be possible eventually.

--- src/main/java/com/limeshulkerbox/bettersodiumvideosettingsbutton/mixin/MixinVideoOptionsScreen.java
+++ src/main/java/com/limeshulkerbox/bettersodiumvideosettingsbutton/mixin/MixinVideoOptionsScreen.java
@@ -3,18 +3,23 @@ package com.limeshulkerbox.bettersodiumvideosettingsbutton.mixin;
 import me.jellysquid.mods.sodium.client.gui.SodiumOptionsGUI;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.VideoOptionsScreen;
+import net.minecraft.client.gui.widget.ButtonListWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.options.Option;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import org.apache.commons.lang3.ArrayUtils;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VideoOptionsScreen.class)
 public class MixinVideoOptionsScreen extends Screen {
+
+
+    @Shadow @Final private static Option[] OPTIONS;
 
     protected MixinVideoOptionsScreen(Text title) {
         super(title);
@@ -26,6 +31,17 @@ public class MixinVideoOptionsScreen extends Screen {
             assert this.client != null;
             this.client.openScreen(new SodiumOptionsGUI(this));
         }));
+    }
+
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonListWidget;addSingleOptionEntry(Lnet/minecraft/client/options/Option;)I", ordinal = 0))
+    private int removeGraphicsButton(ButtonListWidget buttonListWidget, Option option) {
+        return 0;
+    }
+
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void removingFullscreenResolution(CallbackInfo ci) {
+        OPTIONS = ArrayUtils.removeElement(OPTIONS, Option.FULLSCREEN);
+        OPTIONS = ArrayUtils.removeElement(OPTIONS, Option.CLOUDS);
     }
 
     @ModifyConstant(method = "init", constant = @Constant(intValue = 100, ordinal = 0))
