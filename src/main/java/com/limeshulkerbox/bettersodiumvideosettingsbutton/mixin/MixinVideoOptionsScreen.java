50aa7c9d0dc302622965d1eadd1927cadb748d23
capnkork <me@capnkork.com>
2021-06-30 00:32:45 -0400

    Upgrade to minecraft 1.17

--- src/main/java/com/limeshulkerbox/bettersodiumvideosettingsbutton/mixin/MixinVideoOptionsScreen.java
+++ src/main/java/com/limeshulkerbox/bettersodiumvideosettingsbutton/mixin/MixinVideoOptionsScreen.java
@@ -1,12 +1,9 @@
 package com.limeshulkerbox.bettersodiumvideosettingsbutton.mixin;
 
-import me.jellysquid.mods.sodium.client.gui.SodiumOptionsGUI;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.VideoOptionsScreen;
+import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonListWidget;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.options.Option;
-import net.minecraft.text.LiteralText;
+import net.minecraft.client.option.Option;
 import net.minecraft.text.Text;
 import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Final;
@@ -17,8 +14,7 @@ import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VideoOptionsScreen.class)
-public class MixinVideoOptionsScreen extends Screen {
-
+public abstract class MixinVideoOptionsScreen extends Screen {
     @Shadow
     @Final
     @Mutable
@@ -30,19 +26,20 @@ public class MixinVideoOptionsScreen extends Screen {
 
     @Inject(method = "init", at = @At("HEAD"))
     void mixinInit(CallbackInfo callbackInfo) {
-        this.addButton(new ButtonWidget(this.width / 2 + 5, this.height - 27, 150, 20, new LiteralText("Sodium Video Settings"), (button) -> {
+        // We can't do this because sodium hasn't released for 1.17 yet
+        /*this.addDrawableChild(new ButtonWidget(this.width / 2 + 5, this.height - 27, 150, 20, new LiteralText("Sodium Video Settings"), (button) -> {
             assert this.client != null;
             this.client.openScreen(new SodiumOptionsGUI(this));
-        }));
+        }));*/
     }
 
-    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonListWidget;addSingleOptionEntry(Lnet/minecraft/client/options/Option;)I", ordinal = 0))
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonListWidget;addSingleOptionEntry(Lnet/minecraft/client/option/Option;)I", ordinal = 0))
     private int removeGraphicsButton(ButtonListWidget buttonListWidget, Option option) {
         return 0;
     }
 
     @Inject(method = "<clinit>", at = @At("TAIL"))
-    private static void removingFullscreenResolution(CallbackInfo ci) {
+    private static void removeOptions(CallbackInfo ci) {
         OPTIONS = ArrayUtils.removeElement(OPTIONS, Option.FULLSCREEN);
         OPTIONS = ArrayUtils.removeElement(OPTIONS, Option.CLOUDS);
     }
