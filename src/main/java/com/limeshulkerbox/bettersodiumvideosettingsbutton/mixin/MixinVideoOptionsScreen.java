1a40fe0ca0428fe7d40be9fbfdcd285ff2c972d0
LimeShulkerBox <77601849+LimeShulkerBox@users.noreply.github.com>
2021-07-05 09:28:29 +1000

    Adds support for multiple languages
    If you know more languages than me (I am english only xD) then you can contribute

--- src/main/java/com/limeshulkerbox/bettersodiumvideosettingsbutton/mixin/MixinVideoOptionsScreen.java
+++ src/main/java/com/limeshulkerbox/bettersodiumvideosettingsbutton/mixin/MixinVideoOptionsScreen.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.option.Option;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
@@ -35,7 +36,7 @@ public abstract class MixinVideoOptionsScreen extends Screen {
 
     @Inject(method = "init", at = @At("HEAD"))
     void mixinInit(CallbackInfo callbackInfo) {
-        this.addDrawableChild(new ButtonWidget(this.width / 2 + 5, this.height - 27, 150, 20, new LiteralText("Sodium Video Settings"), (button) -> {
+        this.addDrawableChild(new ButtonWidget(this.width / 2 + 5, this.height - 27, 150, 20, new TranslatableText("text.bettersodiumvideosettings.sodiumvideosettings"), (button) -> {
             assert this.client != null;
             if (FabricLoader.getInstance().isModLoaded("sodium-extra")) {
                 if (FabricLoader.getInstance().isModLoaded("iris")) {
